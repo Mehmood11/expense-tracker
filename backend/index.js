efdc268de179b363b618c04a91ee7751e7bc4076
Mehmood Khan <khan5207403@gmail.com>
2025-02-27 12:32:58 +0500

    Passport configured on BE

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,53 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import { buildContext } from "graphql-passport";
+
 import mergedTypeDefs from "./type-defs/index.js";
 import mergeResolvers from "./resolvers/index.js";
 import { connectDB } from "./db/connect-db.js";
+import { configurePassport } from "./passport/passport.config.js";
 
 dotenv.config();
+configurePassport();
+
 const app = express();
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // this option specifies whether to save the session to the store on every request
+    saveUninitialized: false, // this option specifies whether to save uninitialized sessions
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
+    },
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -25,10 +60,13 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
 
